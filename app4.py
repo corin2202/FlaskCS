96e1ad737ac45bec2b9b9b516f7577f5f02d9d98
Corin Yelland <YellandCJ@cardiff.ac.uk>
2025-04-18 12:26:34 +0100

    Made basket page nicer and added checkout success

--- app4.py
+++ app4.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
-from wtforms.validators import NumberRange, Optional
+from wtforms import IntegerField, SubmitField, StringField, ValidationError
+from wtforms.validators import NumberRange, Optional, DataRequired
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+import re
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -21,20 +22,51 @@ class Pizza(db.Model):
     description = db.Column(db.Text)
     image = db.Column(db.String(40))
     veggie = db.Column(db.Boolean)
+    side = db.Column(db.Boolean)
 
 
 
 
 class ChoiceForm(FlaskForm):
-    quantity = IntegerField("Enter the number of pizzas:",validators = [
+    quantity = IntegerField("Enter the desired amount:",validators = [
         Optional(),
         NumberRange(min = 1,message = "Please enter a number greater than 0")
     ])
 
     submit = SubmitField('Submit')
 
+def card_number_valid(form,field):
+    # checks for 3 sets of 4 digits, followed by either space or hyphen, then checks for last 4 no space or hyphen on the end
+    if re.match(r'^([0-9]{4}(-|\s)?){3}[0-9]{4}$',field.data) == None:
+        raise ValidationError("Card does not match correct format")
+    # performs luhn checksum, in reverse order
+    # add the digit to total if odd index, if even double it and add it
+    # but if doubled result 2 digits long, add sum of digits to total
+    # multiple of 10 is valid card number
+    index = 1
+    total = 0
+    for num in field.data[::-1]:
+        if num != "-" and num != " ":
+            if index % 2 == 0:
+                doubled = int(num) * 2
+                if doubled > 9:
+                    for digit in str(doubled):
+                        total += int(digit)
+                else:
+                    total += doubled
+            else:
+                total += int(num)
+            index += 1
+
+    if total % 10 != 0:
+        raise ValidationError("Card is not a valid number sequence")
 
 
+class CardForm(FlaskForm):
+    card_number = StringField("Card number", [DataRequired(), card_number_valid])
+    submit = SubmitField('Submit')
+
+    
 
 
 @app.route('/')
@@ -79,13 +111,16 @@ def singleProductPage(pizzaName):
     return "Pizza not found", 404
 
 
-@app.route('/basket')
+@app.route('/basket', methods = ["GET","POST"])
 def basketPage():
     # stores a basketItems as many of pizzaId: quantity
     basketItems = []
     total_price = 0
+    
+
 
     if "basket" in session.keys():
+        checkout = request.form.get('checkout_value')
         basket = session["basket"]
         for pizza_id, quantity in basket.items():
             pizza = Pizza.query.get(int(pizza_id))
@@ -97,8 +132,23 @@ def basketPage():
                     "quantity": quantity
                 })
 
+        if checkout == "yes":
+            return redirect(url_for('checkout',total_price = total_price))
+
+
     return render_template('basket.html', basket = basketItems, total_price = round(total_price,2))
     
+
+@app.route('/checkout/<total_price>', methods = ["GET","POST"])
+def checkout(total_price):
+    form = CardForm()
+
+    if form.validate_on_submit():
+        # clear basket and send to new page
+        return render_template('checkoutSuccess.html')
+    
+    return render_template('checkout.html', total_price = total_price, form = form)
+
 
 @app.route('/update_quantity', methods = ["POST"])
 def update_quantity():
--- populateDB.py
+++ populateDB.py
@@ -1,18 +1,23 @@
 from app4 import app,db,Pizza
 
 pizzas = [
-    { "name": "Margarita", "price": "£15.99","description": "Cheese and tomato", "image": "margarita.jpg", "veggie": False },
-    { "name": "Pepperoni","price": "£17.99",  "description": "Pepperoni, cheese and tomato", "image" : "pepperoni.jpg", "veggie": False },
-    { "name": "Hawaiian", "price":"£17.99", "description": "Pineapple, ham, cheese and tomato", "image": "hawaiian.jpg", "veggie": False },
-    { "name": "Onion", "price": "£13.99", "description": "Onion", "image": "onion.jpg", "veggie": True},
-    { "name": "Chocolate chicken", "price": "£20.99", "description": "Chocolate sprinkles and smarties with roast chicken", "image": "chocchicken.jpg", "veggie": False},
-    { "name": "None pizza left beef", "price": "£9.99", "description": "Beef on the left", "image": "none.webp", "veggie": False},
-    { "name": "Kiwi", "price": "£13.99", "description": "Kiwi and cheese", "image": "kiwi.jpg", "veggie": True},
-    { "name": "Tomato", "price": "£8.99", "description": "Tomato", "image": "tomato.webp", "veggie": True},
-    { "name": "Zucchini", "price": "£13.99", "description": "Zucchini and cheese", "image": "zucchini.webp", "veggie": True},
-    { "name": "Pea-zza", "price": "£12.99", "description": "Peas cress and dirt", "image": "green.webp", "veggie": True},
-    { "name": "Primitive", "price": "£4.99", "description": "...", "image": "base.jpg", "veggie": True},
-    { "name": "Troll", "price": "£14.99", "description": "Beans and olives", "image": "troll.jpg", "veggie": True},
+    { "name": "Margarita", "price": "£15.99","description": "Cheese and tomato", "image": "margarita.jpg", "veggie": False, "side": False},
+    { "name": "Pepperoni","price": "£17.99",  "description": "Pepperoni, cheese and tomato", "image" : "pepperoni.jpg", "veggie": False, "side": False },
+    { "name": "Hawaiian", "price":"£17.99", "description": "Pineapple, ham, cheese and tomato", "image": "hawaiian.jpg", "veggie": False, "side": False },
+    { "name": "Onion", "price": "£13.99", "description": "Onion", "image": "onion.jpg", "veggie": True, "side": False},
+    { "name": "Chocolate chicken", "price": "£20.99", "description": "Chocolate sprinkles and smarties with roast chicken", "image": "chocchicken.jpg", "veggie": False, "side": False},
+    { "name": "None pizza left beef", "price": "£9.99", "description": "Beef on the left", "image": "none.webp", "veggie": False, "side": False},
+    { "name": "Kiwi", "price": "£13.99", "description": "Kiwi and cheese", "image": "kiwi.jpg", "veggie": True, "side": False},
+    { "name": "Tomato", "price": "£8.99", "description": "Tomato", "image": "tomato.webp", "veggie": True, "side": False},
+    { "name": "Zucchini", "price": "£13.99", "description": "Zucchini and cheese", "image": "zucchini.webp", "veggie": True, "side": False},
+    { "name": "Pea-zza", "price": "£12.99", "description": "Peas cress and dirt", "image": "green.webp", "veggie": True, "side": False},
+    { "name": "Primitive", "price": "£4.99", "description": "...", "image": "base.jpg", "veggie": True, "side": False},
+    { "name": "Troll", "price": "£14.99", "description": "Beans and olives", "image": "troll.jpg", "veggie": True, "side": False},
+    { "name": "Mozzarella sticks", "price": "£5.99", "description": "Mozzarella deep fried", "image": "mozzarella.jpg", "veggie": False, "side": True},
+    { "name": "Garlic dip", "price": "£2.99", "description": "Garlic flavoured mayonnaise", "image": "garlicdip.png", "veggie": False, "side": True},
+    { "name": "Garlic bread", "price": "£3.99", "description": "Garlic toasted flatbread", "image": "garlicbread.jpg", "veggie": False, "side": True},
+    { "name": "Chicken strips", "price": "£5.99", "description": "Medium rare chicken strips", "image": "chickenstrips.jpg", "veggie": False, "side": True},
+
 
 ]
 
@@ -21,7 +26,7 @@ with app.app_context():
     db.drop_all()
     db.create_all()
     for pizza in pizzas:
-        newPizza = Pizza(name = pizza["name"], price = pizza["price"], description = pizza["description"], image = pizza["image"], veggie = pizza["veggie"])
+        newPizza = Pizza(name = pizza["name"], price = pizza["price"], description = pizza["description"], image = pizza["image"], veggie = pizza["veggie"], side = pizza["side"])
         db.session.add(newPizza)
 
     db.session.commit()
